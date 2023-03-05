366080c380d706842bdc7211080fb75b7878789a
Matheus Alves <mathalves07.winstore@outlook.com>
2023-03-05 16:21:28 -0300

    Created the solution to control dishes and it's categories, and also created the domain classes and interfaces needed

---
+++ src/Dishes.App/Dishes.Domain/Entities/Dish.cs
@@ -0,0 +1,33 @@
+﻿using Dishes.Domain.Entities.Base;
+using Dishes.Domain.Validation;
+
+namespace Dishes.Domain.Entities;
+
+public class Dish : BaseEntity
+{
+    public bool IsAvailable { get; private set; } = false;
+    public Decimal Price { get; private set; }
+
+    public Dish(Guid id, string name, DateTime createdAt, string createdBy, bool isAvailable, Decimal price): base(id, name, createdAt, createdBy)
+    {
+        IsAvailable = isAvailable;
+
+        validate(price);
+    }
+
+    public Dish(Guid id, string name, DateTime createdAt, string createdBy, DateTime updatedAt,  string updatedBy, bool isAvailable, Decimal price): base(id, name, createdAt, createdBy, updatedAt, updatedBy)
+    {
+        IsAvailable = isAvailable;
+
+        validate(price);
+    }
+
+    private void validate(Decimal price)
+    {
+        DomainExceptionValidation.When(string.IsNullOrEmpty(price.ToString()), "Price can not be null. Please, provide a valid price.");
+        DomainExceptionValidation.When(Decimal.IsNegative(price), "Price can not be smaller than 0(zero). Please, provide a valid price.");
+        DomainExceptionValidation.When(Decimal.Zero == price, "Price can not be 0(zero). Please, provide a valid price.");
+
+        Price = price;
+    }
+}
