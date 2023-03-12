512af4c89a1524a057db66c7b7142ef3813d9421
Matheus Alves <mathalves07.winstore@outlook.com>
2023-03-12 16:11:01 -0300

    Removed the entire project, due to architectural error

--- src/Dishes.App/Dishes.Domain/Interfaces/ICategoryRepository.cs
+++
@@ -1,6 +0,0 @@
-﻿using Dishes.Domain.Entities;
-using Dishes.Domain.Interfaces.Generic;
-
-namespace Dishes.Domain.Interfaces;
-
-public interface ICategoryRepository: IGenericRepository<Category> { }
